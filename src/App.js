51ccdf4021e2fac8f12ebf2fab2235383d837b5b
olecgema <165628987+olecgema@users.noreply.github.com>
2024-09-18 11:48:41 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+const JOB_KEY = "JOB_LIST";
+
+function App() {
+  const [job, setJob] = useState("");
+  const [jobList, setJobList] = useState(() => {
+    const storageJobList = JSON.parse(localStorage.getItem(JOB_KEY));
+    // Đảm bảo giá trị trả về là một mảng
+    return Array.isArray(storageJobList) ? storageJobList : [];
+  });
+
+  const removeJob = (jobToRemove) => {
+    setJobList((prevJobList) => {
+      // Đảm bảo prevJobList luôn là mảng trước khi gọi filter
+      const newJobList = Array.isArray(prevJobList) 
+        ? prevJobList.filter((item) => item !== jobToRemove) 
+        : [];
+
+      localStorage.setItem(JOB_KEY, JSON.stringify(newJobList));
+      return newJobList;
+    });
+  };
+
+  const handleAddJob = () => {
+    if (job === "") {
+      return;
+    }
+
+    setJobList((prevState) => {
+      // Đảm bảo prevState luôn là mảng
+      const newJobList = Array.isArray(prevState) 
+        ? [...prevState, job] 
+        : [job];
+
+      localStorage.setItem(JOB_KEY, JSON.stringify(newJobList));
+
+      return newJobList;
+    });
+
+    setJob("");
+  };
+
+  return (
+    <div className="App" style={{ padding: 20 }}>
+      <div style={{ border: "1px solid yellowgreen", padding: 20 }}>
+        <input value={job} onChange={(e) => setJob(e.target.value)} />
+        <button onClick={handleAddJob}>Add</button>
+        <ul>
+          {jobList.map((job, index) => (
+            <li key={index}>
+              {job}
+              <button onClick={() => removeJob(job)}>Xóa</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
